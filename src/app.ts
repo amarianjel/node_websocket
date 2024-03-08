e5068bc239ca3f2dc8d530a98051ed985118aa36
amarianjel <abrm.enginner@gmail.com>
2024-03-08 16:12:10 -0300

    Websocker Libreria

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("first ad")
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+    console.log("Cliente conectado com sucess")
+
+    ws.on('error', console.error);
+
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('Hola desde el sevidor! on port 3000');
+
+    setInterval(() => {
+        ws.send('Hola cada 2 seg');
+    }, 2000)
+});
