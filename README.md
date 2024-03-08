4a254c75b291a460c99314ab32bfcabefe9b6d08
amarianjel <abrm.enginner@gmail.com>
2024-03-08 17:17:47 -0300

    Client Broadcast - A todos menos el emisor

---
+++ README.md
@@ -0,0 +1,6 @@
+(Libreria)[https://developer.mozilla.org/en-US/docs/Web/API/WebSocket]
+
+WS
+```npm i ws```
+
+https://www.npmjs.com/package/ws
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
+
     console.log("Cliente conectado com sucess")
 
     ws.on('error', console.error);
 
-
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
+
+        const payload = JSON.stringify({
+            type: 'custom-message',
+            payload: data.toString(),
+        })
+        // ws.send( JSON.stringify(payload) );
+    
+        //* Todos - incluyente
+        // wss.clients.forEach(function each(client) {
+        //     if (client.readyState === WebSocket.OPEN) {
+        //         client.send(payload, { binary: false });
+        //     }
+        // });
+    
+        // * Todos excluyente
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(payload, { binary: false });
+            }
+        });
     });
 
-    ws.send('Hola desde el sevidor! on port 3000');
+    // setInterval(() => {
+    //     ws.send('Hola cada 2 seg');
+    // }, 2000)
 
-    setInterval(() => {
-        ws.send('Hola cada 2 seg');
-    }, 2000)
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    })
 });
